309b369611d1a3cbfeff0e0198e182ea0e0dab5b
russelljohnwilliams <russellwilliamscc@gmail.com>
2018-03-02 21:17:55 +0000

    adds posts and pages to a single page

--- wp-content/themes/nataliebarrett_wp/index.php
+++ wp-content/themes/nataliebarrett_wp/index.php
@@ -16,23 +16,47 @@ get_header(); ?>
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
-		
-		<?php query_posts('post_type=page&order=ASC'); ?>	
-		
+
+	<div class="hidden-posts-to-clone">
+
 		<?php
 		
 		if ( have_posts() ) :
 			if ( is_home() && ! is_front_page() ) : ?>
+		<header>
+			<h1 class="page-title screen-reader-text">
+
+				// <?php single_post_title(); ?>
+
+			</h1>
+		</header>
 
 		<?php
 
 		endif;
 
 		/* Start the Loop */
-		while ( have_posts() ) : the_post();  ?>
+		while ( have_posts() ) : the_post();
 
+					/*
+					 * Include the Post-Format-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+					get_template_part( 'template-parts/content', get_post_format() );
 
+					endwhile;
+
+					the_posts_navigation();
+					else :
+						get_template_part( 'template-parts/content', 'none' );
+
+					endif; ?>
+				</div>
+
+		<?php query_posts('post_type=page&order=ASC'); ?>	
 		
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 			<div class="page-section" id=
 			
@@ -44,93 +68,47 @@ get_header(); ?>
 			</div>
 			<div class="section-content">
 				<?php the_content(); ?>
-				</div>	
-			<div class="quote-block">
-				<?php the_field('quote-block'); ?>
-				</div>
-		</div>
-	<?php 		endwhile;
-
-					the_posts_navigation();
-					else :
-						get_template_part( 'template-parts/content', 'none' );
-
-					endif; ?>
-				</main><!-- #main -->
-			</div><!-- #primary -->
-
-
-
-
-
-
-
-			<div class="hidden-posts">
-				
-				<?php
-				
-				if ( have_posts() ) :
-					if ( is_home() && ! is_front_page() ) : ?>
-
-				<?php
-
-				endif;
-
-				/* Start the Loop */
-				while ( have_posts() ) : the_post();  ?>
-
-
-				
-
-					<div class= "hidden"+
-					
-					<?php the_title(); ?>
-					
-					>
-				<main id="main" class="site-main" role="main">
-
-						<?php
-						
-						if ( have_posts() ) :
-							if ( is_home() && ! is_front_page() ) : ?>
-						<header>
-							<h1 class="page-title screen-reader-text">
-
-								// <?php single_post_title(); ?>
-
-							</h1>
-						</header>
-
-						<?php
-
-						endif;
-
-						/* Start the Loop */
-						while ( have_posts() ) : the_post();
-
-									/*
-									 * Include the Post-Format-specific template for the content.
-									 * If you want to override this in a child theme, then include a file
-									 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-									 */
-									get_template_part( 'template-parts/content', get_post_format() );
-
-									endwhile;
-
-									the_posts_navigation();
-									else :
-										get_template_part( 'template-parts/content', 'none' );
-
-									endif; ?>
-
-								</main><!-- #main -->
-			<?php endwhile; endif; ?>
-
 			</div>
+			<div class="quote-content">
+				<?php the_field('quote-content'); ?>
+			</div>
+		</div>
+
+	<?php endwhile; endif; ?>
 
 
 
-	<?php
-	get_sidebar();
-	get_footer();
-	
+
+
+			</main><!-- #main -->
+		</div><!-- #primary -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		<?php
+		get_sidebar();
+		get_footer();
