169204f90457c4f6eac08f7a59ef74af363b3c0f
russelljohnwilliams <russellwilliamscc@gmail.com>
2018-03-08 00:21:01 +0000

    shows / hides menu on scroll up / down

--- wp-content/themes/nataliebarrett_wp/footer.php
+++ wp-content/themes/nataliebarrett_wp/footer.php
@@ -15,15 +15,7 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'nataliebarrett_wp' ) ); ?>"><?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'nataliebarrett_wp' ), 'WordPress' );
-			?></a>
-			<span class="sep"> | </span>
-			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'nataliebarrett_wp' ), 'nataliebarrett_wp', '<a href="http://underscores.me/">Underscores.me</a>' );
-			?>
+			<div class='dispose'></div>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
--- wp-content/themes/nataliebarrett_wp/js/jquery-script.js
+++ wp-content/themes/nataliebarrett_wp/js/jquery-script.js
@@ -1,20 +1,42 @@
+
+
 jQuery( document ).ready(function() {
   cloneContent()
   jQuery(this).scrollTop(0);
-
   removeHrefFromMenu()
-  //Just pass the jQuery object
-  background_image_parallax(jQuery("#natalie .quote-content"), 0.2);
+  background_image_parallax(jQuery("#natalie .quote-content"), 2);
+  // background_image_parallax(jQuery('#about .quote-content'), 2);
+  // background_image_parallax(jQuery("#natalie .section-title"), -5);
+  // background_image_parallax(jQuery("#work"), 1);
+  fadeInContentOnScroll('.cloned-content .post ')
+  fadeInContentOnScroll('#about .quote-content')
 
-  //optional second value for speed
-  // background_image_parallax(jQuery(".box3"), 0.25);
-  fadeInContentOnScroll('.cloned-content .post')
   fadeInContent('.section-content')
   setTimeout(function(){
-    fadeInContent('.quote-content')
+    fadeInContent('#natalie .quote-content', 'padding-top')
   }, 500)
-
 })
+
+var lastScrollTop = 0;
+
+jQuery(window).on('scroll', function() {
+  st = jQuery(this).scrollTop();
+  if(st < lastScrollTop) {
+    jQuery('#primary-menu').fadeIn()
+  }
+  else {
+    jQuery('#primary-menu').fadeOut()
+  }
+  lastScrollTop = st;
+});
+
+
+jQuery(function(){
+  jQuery('#site-navigation').mouseover(function(){
+    jQuery('#primary-menu').fadeIn()
+  })
+})
+
 
 function cloneContent(){
   jQuery(".content-to-clone").clone().removeAttr('class').attr('class', "cloned-content content-to-clone").appendTo('#work .section-content-wrapper');
@@ -30,50 +52,58 @@ jQuery(function() {
     var splitText = this.innerText.split(' ');
     var hashAnchor = "#" + splitText
     var targetAnchor = jQuery(hashAnchor).offset().top
-    jQuery('html, body').animate({scrollTop: targetAnchor}, 1000);
+    jQuery('html, body').animate({scrollTop: targetAnchor }, 1000);
   });
 });
 
 function fadeInContentOnScroll(content){
   jQuery(window).scroll( function(){
     jQuery(content).each( function(i){
+
       var bottom_of_object = jQuery(this).offset().top + jQuery(this).outerHeight();
       var bottom_of_window = jQuery(window).scrollTop() + jQuery(window).height();
       if( bottom_of_window > bottom_of_object ){
         fadeInContent(this)
+
       }
     })
   })
 }
 
 function fadeInContent(content){
-  jQuery(content).animate({'opacity':'1'},1500);
+  jQuery(content).animate({'opacity':'1', 'top': '0'},1500); 
 }
 
+// function direction_of_fade(object){
+//   object.css({direction : (0) });
+// }
 
+function background_image_parallax(object, speed){
+  jQuery(window).scroll(function(){
+    var doc = jQuery(document).scrollTop()
+    // console.log('top', object.css('top'))
+    // var doc = jQuery(object.parent()).scrollTop()
+        // console.log("content: ", jQuery(object).scrollTop())
 
-
-
-
-
-
-
+    // #### needs to change document to be the top of the current section, 
+    // #### once the top of the section is above the top of the screen, paralax can begin
+    object.css({"top" : (doc / (-speed )) });
+  });
+};
 
 
 // function background_image_parallax(object, multiplier){
-//     multiplier = typeof multiplier !== 'undefined' ? multiplier : 0.7;
-//     multiplier = 1 + multiplier;
-//     var padding = jQuery(object).css('padding-top')
-//     var doc = jQuery(document);
-//     jQuery(window).scroll(function(){
-//       var from_top = doc.scrollTop()
-//           var parse = parseFloat(padding);
-//           var sum = (parse - (from_top / 2) )
-//       object.css({"top" : sum });
-     
-//     });
-// };
+//   // multiplier = typeof multiplier !== 'undefined' ? multiplier : 0.7;
+//   // multiplier = 1 + multiplier;
+//   var doc = jQuery(document);
+//   jQuery(window).scroll(function(){
+//     var doc_from_top = jQuery(document).scrollTop()
+//     // console.log('quote-content', jQuery('.quote-content').css('top'))
 
+//     object.css({"top" : (doc_from_top / (-1 )) });
+
+//   });
+// };
 
 
 
